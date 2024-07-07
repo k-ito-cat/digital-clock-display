4ad83f0db82793bd693fca6f484bf160876494a1
k-ito <kyohy163@gmail.com>
2024-07-07 20:54:01 +0900

    chore: コマンド追加

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world! 2</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>Hello</p>
     </>
   );
 }
