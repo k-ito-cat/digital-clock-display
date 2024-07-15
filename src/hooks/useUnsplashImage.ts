aeebd884fc4e15b644a9812108d750f26020611e
k-ito <kyohy163@gmail.com>
2024-07-15 20:32:17 +0900

    feat: 初回フェッチと定期的にフェッチを行う処理の実装

--- src/hooks/useUnsplashImage.ts
+++ src/hooks/useUnsplashImage.ts
@@ -1,29 +1,74 @@
 import { useEffect, useState } from "react";
-import { getUnsplashImage } from "~/api/getUnsplashImage";
+import { useCookies } from "react-cookie";
+import { getUnsplashRandomImageUrl } from "~/api/getUnsplashRandomImageUrl";
 import { FETCH_INTERVAL } from "~/constants/intervalTime";
+import {
+  COOKIE_KEY,
+  STORAGE_KEY_FETCH_TIMESTAMP,
+  STORAGE_KEY_IMAGE_CACHE,
+} from "~/constants/keyName";
 
-export const useUnsplashImage = async () => {
-  // TODO: altなど他情報も取る
+// TODO: 機能として、セッションかcookieかは選びたい（タブを新しく開いたときどうするかとかで）
+
+export const useUnsplashImage = () => {
   const [photo, setPhoto] = useState<string | null>(null);
+  const [cookies, setCookie] = useCookies([COOKIE_KEY]);
+
+  /**
+   * フェッチ直後の時刻と画像URLをローカルストレージに保存
+   * @param imageUrl
+   */
+  const setFetchData = (imageUrl: string) => {
+    localStorage.setItem(STORAGE_KEY_IMAGE_CACHE, imageUrl);
+    localStorage.setItem(STORAGE_KEY_FETCH_TIMESTAMP, String(Date.now()));
+  };
 
   useEffect(() => {
-    const firstFetch = async () => {
-      const SESSION_KEY = "fetch_expiry_time";
-      const expiryTime = sessionStorage.getItem(SESSION_KEY);
-      if (expiryTime) return;
+    // NOTE: 初回フェッチ後にリロードすることによる再フェッチの防止として、
+    // 初回フェッチ後に永続的なcookieを付与し、cookieが存在する場合は再フェッチを行わず、ローカルストレージに保存された画像を読み取る
 
-      sessionStorage.setItem(SESSION_KEY, String(Date.now()));
-      setPhoto(await getUnsplashImage("nature"));
+    const firstFetch = async () => {
+      if (!cookies.initial_load_completed) {
+        // 永続的なcookieを付与
+        setCookie(COOKIE_KEY, true, { path: "/" });
+
+        // サイトを始めて訪れたときのみフェッチを行う
+        const initialFetchResponse: string = await getUnsplashRandomImageUrl();
+
+        setFetchData(initialFetchResponse);
+        setPhoto(initialFetchResponse);
+      } else {
+        const imageUrl = localStorage.getItem(STORAGE_KEY_IMAGE_CACHE);
+
+        if (imageUrl) {
+          setPhoto(imageUrl);
+        } else {
+          localStorage.setItem(STORAGE_KEY_IMAGE_CACHE, photo || "");
+        }
+      }
     };
     firstFetch();
-
-    // 定期的な時間間隔で画像を更新する
-    const intervalId = setInterval(async () => {
-      setPhoto(await getUnsplashImage("nature"));
-    }, FETCH_INTERVAL);
-
-    () => clearInterval(intervalId);
   }, []);
+
+  /**
+   * 1秒ごとにフェッチ直後の時刻+インターバル時間と現在時刻を比較し、
+   * 現在時刻がフェッチ直後の時刻+インターバル時間を超えていた場合、再フェッチを行う
+   */
+  const intervalId = setInterval(async () => {
+    const fetchTimestamp = localStorage.getItem(STORAGE_KEY_FETCH_TIMESTAMP);
+
+    if (!fetchTimestamp) return;
+
+    const refetchAt = Number(fetchTimestamp) + FETCH_INTERVAL;
+    if (Date.now() >= refetchAt) {
+      // 前回のフェッチの時間+インターバルの時間を現在時刻が超えた場合、再フェッチを行う
+      const updateImageUrl: string = await getUnsplashRandomImageUrl();
+
+      setFetchData(updateImageUrl);
+      setPhoto(updateImageUrl);
+    }
+    return () => clearInterval(intervalId);
+  }, 1000);
 
   return photo;
 };
