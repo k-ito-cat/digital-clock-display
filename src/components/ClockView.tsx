a4a1c1cb5f8c7998a463fd943ede5055bbcace3a
k-ito <kyohy163@gmail.com>
2024-07-15 20:24:18 +0900

    style: 時計UIの微調整

--- src/components/ClockView.tsx
+++ src/components/ClockView.tsx
@@ -5,7 +5,7 @@ export const ClockView = () => {
   const currentTime = useCurrentTime();
 
   return (
-    <p className="border-4 px-4 py-2 text-4xl w-[220px] text-center">
+    <p className="border-[10px] border-white px-4 py-2 text-white border-opacity-50 text-[64px] text-center">
       {currentTime}
     </p>
   );
