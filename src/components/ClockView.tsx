066338a793ef7d533930f01d1fe49ae7f6b00737
k-ito <kyohy163@gmail.com>
2024-08-04 19:30:03 +0900

    chore: UI調整

--- src/components/ClockView.tsx
+++ src/components/ClockView.tsx
@@ -5,8 +5,10 @@ export const ClockView = () => {
   const currentTime = useCurrentTime();
 
   return (
-    <p className="border-[10px] border-white px-4 py-2 text-white border-opacity-50 text-[64px] text-center">
-      {currentTime}
-    </p>
+    <div className="border-[10px] w-[400px] border-white px-4 py-2">
+      <p className="tracking-[.1em] text-white text-[64px] text-center">
+        {currentTime}
+      </p>
+    </div>
   );
 };
