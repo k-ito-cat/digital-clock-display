5906507fb8a0c7e1c1473d46b3663de2c5203d31
k-ito <kyohy163@gmail.com>
2024-08-06 17:54:36 +0900

    feat: ドロワーメニューUIを最小構成に調整しつつ追加

--- src/pages/ClockPage.tsx
+++ src/pages/ClockPage.tsx
@@ -1,9 +1,13 @@
 import { ClockBgImage } from "~/components/ClockBgImage";
 import { ClockView } from "~/components/ClockView";
+import SettingDrawer from "~/components/SettingDrawer";
 
 const ClockPage = () => {
   return (
     <ClockBgImage>
+      <div className="absolute bottom-2 right-0 hover:opacity-50 transition duration-300">
+        <SettingDrawer />
+      </div>
       <ClockView />
     </ClockBgImage>
   );
