35c89e0266c434a56f9318515b14fd374f3bfd19
k-ito <kyohy163@gmail.com>
2024-08-25 20:57:08 +0900

    add: formatterの導入と整形

--- src/components/ClockView.tsx
+++ src/components/ClockView.tsx
@@ -5,8 +5,8 @@ export const ClockView = () => {
   const currentTime = useCurrentTime();
 
   return (
-    <div className="border-[10px] w-[400px] border-white px-4 py-2">
-      <p className="tracking-[.1em] text-white text-[64px] text-center">
+    <div className="w-[400px] border-[10px] border-white px-4 py-2">
+      <p className="text-center text-[64px] tracking-[.1em] text-white">
         {currentTime}
       </p>
     </div>
