9cbc2865741646c3f118b3b88e403080e099e722
k-ito <kyohy163@gmail.com>
2024-09-02 09:17:05 +0900

    feat: カーソル制御の実装

--- src/components/ClockBgImage.tsx
+++ src/components/ClockBgImage.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import { useUnsplashImage } from "~/hooks/useUnsplashImage";
+import { useCursor } from "~/hooks/useCursor";
 
 interface WrapperProps {
   children: ReactNode;
@@ -8,15 +9,21 @@ export const ClockBgImage: React.FC<WrapperProps> = ({ children }) => {
   const photoUrl = useUnsplashImage();
   const [photo, setPhoto] = useState<string | null>(null);
 
+  const { autoHideCursor } = useCursor();
+
   useEffect(() => {
     const getPhoto = async () => {
       setPhoto(await photoUrl);
     };
     getPhoto();
+
+    const setting = false; // これは設定値のモック
+    autoHideCursor(setting, 3);
   }, [photoUrl]);
 
   return (
     <div
+      id="clock-bg-image"
       className={
         "flex h-[100vh] w-full items-center justify-center bg-cover bg-center"
       }
