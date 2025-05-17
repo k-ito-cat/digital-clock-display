c63e3b413588fc7a43d6a04b03c86a5274a19d94
k-ito-cat <kyohy163@gmail.com>
2025-05-17 19:54:21 +0900

    add: ogp, touch-iconの追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,38 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/digital-clock-display/",
+  base: '/digital-clock-display/',
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'touch-icon.png'],
       manifest: {
-        name: "Digital Clock Display",
-        short_name: "Digital Clock",
-        theme_color: "#000000",
-        background_color: "#000000",
-        display: "standalone",
-        start_url: "/digital-clock-display/",
+        name: 'Digital Clock Display',
+        short_name: 'Digital Clock',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        start_url: '/digital-clock-display/',
         icons: [
           {
-            src: "/digital-clock-display/favicon.svg",
-            sizes: "192x192",
-            type: "image/svg",
+            src: '/digital-clock-display/favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg',
           },
           {
-            src: "/digital-clock-display/apple-touch-icon.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: '/digital-clock-display/touch-icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/digital-clock-display/touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
         ],
       },
@@ -37,7 +43,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "src"),
+      '~': path.resolve(__dirname, 'src'),
     },
   },
 });
